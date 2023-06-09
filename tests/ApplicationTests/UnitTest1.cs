6f5f570ee05bbdf775b77f4cef8f6a115097f36b
developertest_02 <developertest02@gmail.com>
2023-06-09 12:19:30 -0400

    Test for getting data working

--- tests/ApplicationTests/UnitTest1.cs
+++ tests/ApplicationTests/UnitTest1.cs
@@ -1,3 +1,7 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.Infrastructure.Data;
+
 namespace ApplicationTests;
 
 public class UnitTest1
@@ -5,6 +9,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        var contextOptions = new DbContextOptionsBuilder<CatalogContext>()
+            .UseSqlServer("Server=(localdb)\\mssqllocaldb;Integrated Security=true;Initial Catalog=Microsoft.eShopOnWeb.CatalogDb;")
+            .Options;
+        var ctx = new CatalogContext(contextOptions);
+        var catalogItems = ctx.CatalogItems.ToList();
+        catalogItems.Count.Should().BeGreaterThan(0);
     }
 }
